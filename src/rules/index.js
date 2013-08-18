f4672242ae4016b5e72ee35f0ecd91bad594717e
ji <ji@ji-PC>
2013-08-18 18:22:19 +0800

    修改引号

--- src/rules/index.js
+++ src/rules/index.js
@@ -234,7 +234,7 @@ module.exports = exports = function(webot){
 /************      大湿       *************/ 
   
   function getDashiDetail(){
-	  var newValue = _.map(torrent.list() , function(value , key){ return key + " (<a href="+value.url +">预览</a>)"}).join("\n");
+	  var newValue = _.map(torrent.list() , function(value , key){ return key + ' (<a href="'+value.url +'">预览</a>)'}).join("\n");
 	  return newValue;
   }
   
@@ -263,7 +263,7 @@ module.exports = exports = function(webot){
 	  var data = torrent.findByName(info.text) ;
 	  // 搜索列表
 	  if(data){
-		  message =  "(<a href="+value.url +">预览</a>)" ;
+		  message =  '(<a href="'+data.url +'">预览</a>)' ;
 		  log.info(info.uid +" answer in dashi = " + info.text );
 	  } else if (/求经典/gi.test(info.text)){
 		  message = '(各种经典截图的种子)  http://t.cn/zQaNS89';
